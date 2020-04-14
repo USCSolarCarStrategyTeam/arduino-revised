836434f9c7c110acbb54fa4459370279477e699c
James Buttler <buttler@usc.edu>
2020-04-14 11:32:10 -0700

    Annotate definitions in defs

--- src/defs.h
+++ src/defs.h
@@ -1,3 +1,3 @@
-#define CACHE_CAP 64
-#define N_INPUT 8
-#define N_OUTPUT 2
+#define CACHE_CAP 64        // Capacity of each Input cache
+#define N_INPUT 8           // Number of Inputs a Reader stores
+#define N_OUTPUT 2          // Number of Outputs a Writer stores
