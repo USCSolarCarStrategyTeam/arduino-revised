10b4af46b04c549df0f9928500e604289583eee4
James Buttler <buttler@usc.edu>
2020-04-14 13:06:14 -0700

    Create Processor struct

---
+++ src/processor.c
@@ -0,0 +1,11 @@
+#include "processor.h"
+
+int process(struct Processor* processor) {
+    // Access reader's data
+
+    // Check for critical failure
+
+    // Convert data into forms needed for Writer
+        // This responsibility /might/ be assigned to writer
+        // Will depend on how we communicate output types
+}
