9520b9af2f2fcdc00fc9e5abec2a8b847854bdbd
James Buttler <buttler@usc.edu>
2020-04-14 13:16:22 -0700

    Add processor into main.c

--- src/main.c
+++ src/main.c
@@ -1,18 +1,22 @@
 #include "reader.h"
-// #include "processor.h"
+#include "processor.h"
 #include "writer.h"
 
 int main(void) {
     cprintf("Reading configuration file...");
 
     // Read any data from files
+    // This may include input and output types
 
     cprintf("Initializing telemetry system...");
 
     // Do any initializations
-    struct Reader reader;
-    // struct Processor processor;
-    struct Writer writer;
+    // Keep these dynamic as long as we expect to be passing them to each other
+    // Else, might just have some components be dynamic
+
+    struct Reader* reader = (struct Reader*)malloc(sizeof(struct Reader));
+    struct Processor* processor = (struct Processor*)malloc(sizeof(struct Processor));
+    struct Writer* writer = (struct Writer*)malloc(sizeof(struct Writer));
 
     cprintf("Telemetry system active");
 
